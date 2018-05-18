23dc89d02e726bf718d1fcb7f597d60a5d3e9840
José L. S. A <alagunasalahaddin@live.com>
2018-05-19 01:39:36 +0300

    CMS: :tada: Added app configuration and admin.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CMSImage
+from .models import CMSRendition
+
+admin.site.register(CMSImage)
+admin.site.register(CMSRendition)
--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
-class CoreConfig(AppConfig):
+class CoreAppConfig(AppConfig):
     name = 'core'
+    label = 'core'
+    verbose_name = _('Core')

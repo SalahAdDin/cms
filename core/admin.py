9a6f288e80cb9a5a1812001bd7b0d88cfdabf3a5
José L. S. A <alagunasalahaddin@live.com>
2018-05-19 15:26:14 +0300

    CMS: :tada: Added global settings.

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
+from .models import GlobalSettings
 from .models import CMSImage
 from .models import CMSRendition
 
+admin.site.register(GlobalSettings)
 admin.site.register(CMSImage)
 admin.site.register(CMSRendition)

3a6acf5fe74c065c8ff435dc6df67d117aad7ea5
José L. S. A <alagunasalahaddin@live.com>
2018-05-21 17:39:59 +0300

    CMS: :fire: Reverts import library.

--- cms/urls.py
+++ cms/urls.py
@@ -72,7 +72,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if not settings.DEBUG:
-    import silk
     urlpatterns += [
         url(r'^silk/', include('silk.urls', namespace='silk'))
     ]
